2fa0628a561bdc048a213452ac11f84fd3828614
theeluwin <theeluwin@gmail.com>
2020-02-22 04:16:44 +0900

    forgot setup version update...

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = (
 
 setup(
     name='samitizer',
-    version='0.1.10',
+    version='0.2.1',
     license='MIT',
     author='Jamie Seol',
     author_email='theeluwin@gmail.com',
